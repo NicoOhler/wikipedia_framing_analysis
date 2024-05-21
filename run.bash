b1d62d6c41546775db6c5db9953996c9aef172eb
NicoOhler <nico.ohler@gmail.com>
2024-05-21 16:07:18 +0200

    Add missing install

--- run.bash
+++ run.bash
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Create and activate conda environment
-conda create -y -c conda-forge -n framing_analysis python=3.10.12 
+conda create -y -c conda-forge -n framing_analysis python=3.10.12 matplotlib
 source $(conda info --base)/etc/profile.d/conda.sh # initialize conda in bash
 conda activate framing_analysis
 
 # Install packages
 yes | pip install framefinder
 conda install -y -c conda-forge sentence-transformers
-    #yes | pip install deep-translator
 conda install -y nltk
     #conda install -y conda-forge::tqdm
 conda install -y anaconda::requests
-    #conda install -y conda-forge::wikipedia-api
+#yes | pip install deep-translator
+#conda install -y conda-forge::wikipedia-api
 
 # Run Python script
 python script.py
